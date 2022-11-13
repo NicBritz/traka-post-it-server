0a25d08d2e6267946b49c0f706dd0f26160f2eb5
nico britz <nico.britz@outlook.com>
2022-11-13 16:49:23 +0000

    add delete routes

--- routes.js
+++ routes.js
@@ -5,14 +5,21 @@ const router = express.Router();
 
 // get all posts
 router.get("/posts", async (req, res) => {
-  const posts = await Post.find();
-  res.send(posts);
+  try {
+    const posts = await Post.find();
+    if (!posts) throw new Error();
+    res.send(posts);
+  } catch {
+    res.status(404);
+    res.send({ error: "No Posts found" });
+  }
 });
 
 // get a post by id
 router.get("/posts/:id", async (req, res) => {
   try {
     const post = await Post.findById({ _id: req.params.id });
+    if (!post) throw new Error();
     res.send(post);
   } catch {
     res.status(404);
@@ -24,6 +31,7 @@ router.get("/posts/:id", async (req, res) => {
 router.get("/users/:id", async (req, res) => {
   try {
     const user = await User.findById({ _id: req.params.id });
+    if (!user) throw new Error();
     res.send(user);
   } catch {
     res.status(404);
@@ -33,22 +41,38 @@ router.get("/users/:id", async (req, res) => {
 
 // get all users
 router.get("/users", async (req, res) => {
-  const users = await User.find();
-  res.send(users);
+  try {
+    const users = await User.find();
+    if (!users) throw new Error();
+    res.send(users);
+  } catch {
+    res.status(404);
+    res.send({ error: "UNo users found" });
+  }
 });
 
 // create a post
 router.post("/posts/create", async (req, res) => {
-  const post = new Post(req.body);
-  await post.save();
-  res.send(post);
+  try {
+    const post = new Post(req.body);
+    await post.save();
+    res.send(post);
+  } catch {
+    res.status(500);
+    res.send({ error: "post not saved!" });
+  }
 });
 
 //create a user
 router.post("/users/create", async (req, res) => {
-  const user = new User(req.body);
-  await user.save();
-  res.send(user);
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.send(user);
+  } catch {
+    res.status(500);
+    res.send({ error: "User not saved!" });
+  }
 });
 
 //update a post
@@ -70,4 +94,42 @@ router.patch("/posts/update/:id", async (req, res) => {
     res.send({ error: "Post not found" });
   }
 });
+//update a user
+router.patch("/users/update/:id", async (req, res) => {
+  try {
+    const user = await User.findById({ _id: req.params.id });
+
+    if (req.body.username) user.username = req.body.username;
+    if (req.body.imageUrl) user.imageUrl = req.body.imageUrl;
+    user.date = Date.now();
+
+    await user.save();
+    res.send(user);
+  } catch {
+    res.status(404);
+    res.send({ error: "User not found" });
+  }
+});
+
+// delete post
+router.delete("/posts/delete/:id", async (req, res) => {
+  try {
+    await Post.deleteOne({ _id: req.params.id });
+    res.send("Deleted post id:" + req.params.id);
+  } catch {
+    res.status(404);
+    res.send({ error: "Post doesn't exist!" });
+  }
+});
+// delete user
+router.delete("/users/delete/:id", async (req, res) => {
+  try {
+    await User.deleteOne({ _id: req.params.id });
+    res.send("Deleted user id:" + req.params.id);
+  } catch {
+    res.status(404);
+    res.send({ error: "User doesn't exist!" });
+  }
+});
+
 module.exports = router;
