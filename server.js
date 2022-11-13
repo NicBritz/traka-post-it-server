1fa5026edc0253adf974d37a7483fad4e1579c11
nico britz <nico.britz@outlook.com>
2022-11-13 13:29:42 +0000

    set database connection using mongoose

--- server.js
+++ server.js
@@ -1,23 +1,29 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
-app.use(require("./routes/posts"));
+// app.use(require("./routes/posts"));
 
-//db connection driver
-const dbo = require("./db/conn");
-
-app.listen(PORT, () => {
-  //perform a database connection
-  dbo.connectToServer(function (err) {
-    if (err) {
-      console.log(err);
-    }
-  });
-
-  console.log("The server is running on port: " + PORT);
+// connect to the database
+mongoose.connect(process.env.DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
+
+// connected db
+const db = mongoose.connection;
+
+db.on("connected", () => {
+  app.listen(PORT, () => {
+    //successful database connection
+    console.log("Connected to mongo database and serving on port:" + PORT);
+  });
+});
+
+// binding connection error
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
