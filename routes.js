68c7c724bf8c35aee13d88c9e0b501981ad01a07
nico britz <nico.britz@outlook.com>
2022-11-13 16:21:09 +0000

    add update posts route

--- routes.js
+++ routes.js
@@ -11,15 +11,24 @@ router.get("/posts", async (req, res) => {
 
 // get a post by id
 router.get("/posts/:id", async (req, res) => {
-  const post = await Post.findById({ _id: req.params.id });
-  res.send(post);
+  try {
+    const post = await Post.findById({ _id: req.params.id });
+    res.send(post);
+  } catch {
+    res.status(404);
+    res.send({ error: "Post not found" });
+  }
 });
 
 // get a user by id
 router.get("/users/:id", async (req, res) => {
-  console.log(req.params.id);
-  const user = await User.findById({ _id: req.params.id });
-  res.send(user);
+  try {
+    const user = await User.findById({ _id: req.params.id });
+    res.send(user);
+  } catch {
+    res.status(404);
+    res.send({ error: "User not found" });
+  }
 });
 
 // get all users
@@ -29,17 +38,36 @@ router.get("/users", async (req, res) => {
 });
 
 // create a post
-router.post("/post", async (req, res) => {
+router.post("/posts/create", async (req, res) => {
   const post = new Post(req.body);
   await post.save();
   res.send(post);
 });
 
 //create a user
-router.post("/user", async (req, res) => {
+router.post("/users/create", async (req, res) => {
   const user = new User(req.body);
   await user.save();
   res.send(user);
 });
 
+//update a post
+router.patch("/posts/update/:id", async (req, res) => {
+  try {
+    const post = await Post.findById({ _id: req.params.id });
+
+    if (req.body.userId) post.userId = req.body.userId;
+    if (req.body.content) post.content = req.body.content;
+    post.date = Date.now();
+    if (req.body.isReply) post.isReply = req.body.isReply;
+    if (req.body.repliedToId) post.repliedToId = req.body.repliedToId;
+    if (req.body.votes) post.votes = req.body.votes;
+
+    await post.save();
+    res.send(post);
+  } catch {
+    res.status(404);
+    res.send({ error: "Post not found" });
+  }
+});
 module.exports = router;
